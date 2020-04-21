30640fcb6b1619d6b70915ac60a2c57c07cbbbdc
Fabricio Magalhães Sena <fabricio.magalhaes@toroinvestimentos.com.br>
2020-04-21 13:04:32 -0300

    Add project files.

---
+++ LaboratoryKakfa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboratoryKakfa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
